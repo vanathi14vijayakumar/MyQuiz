f72d8ebbffc8720ef7fef7eb6519c716bc8e66ba
vanathi14vijayakumar <71445914+vanathi14vijayakumar@users.noreply.github.com>
2021-01-30 09:36:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MyQuiz
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,29 @@
+var canvas;
+var gameStage = 0,db
+var playerCounter =0
+var allPlayers,player,game,game1,form
+
+function setup(){
+  canvas = createCanvas(850,400);
+  db = firebase.database();
+  game = new Quiz
+  game1 = new Question;
+  game.getState()
+  game.start()
+
+}
+
+
+function draw(){
+  background("pink");
+  
+
+  if(playerCounter ==4){
+    game.update(1)
+  }
+  
+  if(gameStage ==1){
+    game.play()
+  }
+  
+}
